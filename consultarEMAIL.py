23ab60ed0b5f37bf2c998b28c4f57faa99fc6e92
João Vitor Pires Veloso <x561956@rede.sp>
2024-06-24 15:53:00 -0300

    24 day

--- consultarEMAIL.py
+++ consultarEMAIL.py
@@ -1,17 +1,14 @@
 import pandas as pd
 
-
-
-
-
 def pegarEMAIL():
-    df_geral = pd.read_excel("c:\\Users\\x561956\\OneDrive - rede.sp\\Documentos\\AutomacaoQuedaRede\\nome_email_bibliotecas.xlsx")
-    for index, linha in df_geral.iterrows():
-
-
-        local = linha["Nome"]
-        data = linha["E-mail"]
-
-        print(local, data)
-
-pegarEMAIL()        
+    try:
+        df_geral = pd.read_excel("c:\\Users\\x561956\\OneDrive - rede.sp\\Documentos\\AutomacaoQuedaRede\\nome_email_bibliotecas.xlsx")
+        email_dict = {}
+        for index, linha in df_geral.iterrows():
+            local = linha["Nome"]
+            email = linha["E-mail"]
+            email_dict[local] = email
+        return email_dict
+    except Exception as e:
+        print(f"Erro ao ler o arquivo Excel: {e}")
+        return None
