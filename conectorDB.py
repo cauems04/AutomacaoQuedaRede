52b920e2c79389ecbc364971e6e4e531d3a0b33f
Cauê Mendonça de Sousa <x540865@rede.sp>
2024-06-25 14:01:24 -0300

    Atualização para consulta de chamados já criados

--- conectorDB.py
+++ conectorDB.py
@@ -1,19 +1,18 @@
 import mysql.connector
-import time
 
-def conectar_banco_de_dados():
+def conectar_banco_de_dados(banco_nome):
     try:
         # Estabeleça a conexão com o banco de dados
         conexao = mysql.connector.connect(
-            host= "10.59.208.241",
-            port= "3306",
-            user= "csmb",
-            password= "0730@info",
-            database= "zabbix"
+            host = "10.59.208.241",
+            port = "3306",
+            user = "csmb",
+            password = "0730@info",
+            database = banco_nome
         )
 
         if conexao.is_connected():
-            print("Conexão bem-sucedida!")
+            print("Conexão bem-sucedida!\nBanco acessado: ", banco_nome)
             return conexao
 
     except mysql.connector.Error as erro:
@@ -21,7 +20,7 @@ def conectar_banco_de_dados():
         return None
 
     except Exception as e:
-        print('Erro: '. e)
+        print('Erro: ', e)
 
 def fechar_conexao(conexao):
     if conexao.is_connected():
