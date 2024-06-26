fe84e4c34693f7e16ab0869bafab132cd9b52f5b
Cauê Mendonça de Sousa <x540865@rede.sp>
2024-06-26 15:12:24 -0300

    Testes realizados - Versão completa

--- consultarEMAIL.py
+++ consultarEMAIL.py
@@ -1,30 +1,17 @@
 import pandas as pd
 
 def pegarEMAIL():
-<<<<<<< HEAD
-    try:
-        df_geral = pd.read_excel("c:\\Users\\x561956\\OneDrive - rede.sp\\Documentos\\AutomacaoQuedaRede\\nome_email_bibliotecas.xlsx")
-        email_dict = {}
-=======
 
     email_dict = {}
     try:
         df_geral = pd.read_excel("c:\\Users\\x540865\\Documents\AutomacaoQuedaRede\\nome_email_bibliotecas.xlsx")
->>>>>>> main
         for index, linha in df_geral.iterrows():
             local = linha["Nome"]
             email = linha["E-mail"]
             email_dict[local] = email
-<<<<<<< HEAD
-=======
-    
->>>>>>> main
+
         return email_dict
     except Exception as e:
         print(f"Erro ao ler o arquivo Excel: {e}")
         return None
-<<<<<<< HEAD
-=======
 
-pegarEMAIL()
->>>>>>> main
