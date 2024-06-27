d94b9a0fa0c5d4f7b4f3f81547fe57453fce707b
cauems04 <136491175+cauems04@users.noreply.github.com>
2024-06-27 14:54:35 -0300

    Update mensagemAusenciaRede.py

--- mensagemAusenciaRede.py
+++ mensagemAusenciaRede.py
@@ -1,4 +1,5 @@
 import win32com.client as win32
+import schedule
 from consultasDB import consultaBanco, insercaoBanco, consultaChamadosAtivos
 from consultarEMAIL import pegarEMAIL
 
@@ -24,45 +25,57 @@ def enviar_mensagens_internet():
     bibliotecas_chamados_ativos = consultaChamadosAtivos()
     dados = consultaBanco()
     email_dict = pegarEMAIL()
+
+    dados.append(['10.59.208.183', '20/03/2024 07:06:41', '00:30:00'])
         
     if email_dict is None:
         print("Erro ao obter emails. Verifique o arquivo Excel.")
         return
 
-    if dados:
+    try:
 
-        for dado in dados:
-            nome_biblioteca = dado[0]
+        if dados:
 
-            if nome_biblioteca.startswith('Biblioteca'):
-                
-                if nome_biblioteca in email_dict:
-                    
-                    if nome_biblioteca not in bibliotecas_chamados_ativos:
-                        dest = 'lseloy@prefeitura.sp.gov.br'
-                        assunto = f'Queda de rede na {nome_biblioteca}'
-                        corpo = (f'Ola, Nosso sistema de monitoramento identificou uma queda na rede de internet da {nome_biblioteca}, a queda ocorreu em {dado[1]}.\n\nDesde já, a cordenadoria do sistema municipal de bibliotecas agradece.')
+            for dado in dados:
+                nome_biblioteca = dado[0]
+
+                if nome_biblioteca.startswith('Biblioteca'):
+                        
+                    if nome_biblioteca in email_dict:
                             
-                        escrever_email(outlook, dest, assunto, corpo, nome_biblioteca)
-                        insercaoBanco(nome_biblioteca)
+                        if nome_biblioteca not in bibliotecas_chamados_ativos:
+                            dest = 'lseloy@prefeitura.sp.gov.br'
+                            assunto = f'Queda de rede na {nome_biblioteca}'
+                            corpo = (f'Ola, Nosso sistema de monitoramento identificou uma queda na rede de internet da {nome_biblioteca}, a queda ocorreu em {dado[1]}.\n\nDesde já, a cordenadoria do sistema municipal de bibliotecas agradece.')
+                                    
+                            escrever_email(outlook, dest, assunto, corpo, nome_biblioteca)
+                            insercaoBanco(nome_biblioteca)
 
-                        dest = email_dict[nome_biblioteca]
-                        assunto = f'Queda de rede na {nome_biblioteca}'
-                        corpo = (f'Olá, aqui é a equipe de TI, gostaria de informar que a {nome_biblioteca} está sem rede de internet, já foi aberto um chamado para reparo e manutenção.\n'
-                                    f'Horário da queda: {dado[1]}\n\nObrigado pela atenção.\n\n*Esta é uma mensagem automática*\nFavor *NÃO* responder.')
+                            dest = email_dict[nome_biblioteca]
+                            assunto = f'Queda de rede na {nome_biblioteca}'
+                            corpo = (f'Olá, aqui é a equipe de TI, gostaria de informar que a {nome_biblioteca} está sem rede de internet, já foi aberto um chamado para reparo e manutenção.\n'
+                                        f'Horário da queda: {dado[1]}\n\nObrigado pela atenção.\n\n*Esta é uma mensagem automática*\nFavor *NÃO* responder.')
+                                    
+                            escrever_email(outlook, dest, assunto, corpo, nome_biblioteca)
+
+                        else:
+                            print(f'Já existe um chamado ativo para a {nome_biblioteca}.\n')
                             
-                        escrever_email(outlook, dest, assunto, corpo, nome_biblioteca)
-
                     else:
-                        print(f'Já existe um chamado ativo para a {nome_biblioteca}')
-                    
+                        print(f'Email para {nome_biblioteca} não encontrado na planilha.\n')
+
                 else:
-                    print(f'Email para {nome_biblioteca} não encontrado na planilha.')
+                    print(f'Conjunto de dados do {dado[0]} não pertence a uma biblioteca.\n')
 
-            else:
-                print(f'Conjunto de dados do {dado[0]} não pertence a uma biblioteca.')
-    else:
-        print('Não há presença de dados.')
+        else:
+            print('Não há presença de dados.\n\n')
+        
+        print('----------------')
+    
+    except Exception as e:
+        print('Erro :', e)
 
+schedule.every(10).minutes.do(enviar_mensagens_internet)
 
-enviar_mensagens_internet()
+while True:
+    schedule.run_pending()
